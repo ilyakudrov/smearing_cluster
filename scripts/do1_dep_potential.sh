c253403cdd3ad04a44a037a283f11732bf8f011a
Ilya <ilyakudrov@yandex.ru>
2021-08-13 13:26:52 +0300

    smearing dependence of potential added

--- scripts/do1_dep_potential.sh
+++ scripts/do1_dep_potential.sh
@@ -13,9 +13,14 @@ stout="0"
 APE_steps="300"
 HYP_steps="2"
 stout_steps="0"
-flux_enabled=1
 wilson_enabled=1
 smearing="HYP2_APE"
+
+T_min=6
+T_max=16
+R_min=4
+R_max=20
+calculation_step_APE=10
 
 source "/lustre/rrcmpi/kudrov/conf/${conf_size}/mu${mu}/parameters"
 
@@ -32,7 +37,7 @@ for j in "${!chains[@]}"; do
 
 #log_path="/home/clusters/rrcmpi/kudrov/smearing_cluster/logs/smearing_test/${conf_type}/${conf_size}/mu${mu}/${chains[j]}"
 #log_path="/home/clusters/rrcmpi/kudrov/smearing_cluster/logs/smearing_test/monopoless/${conf_type}/${conf_size}/mu${mu}/${chains[j]}"
-log_path="/home/clusters/rrcmpi/kudrov/smearing_cluster/logs/smearing_test/wilson_loop/${monopole}/${conf_type}/${conf_size}/mu${mu}/${chains[j]}"
+log_path="/home/clusters/rrcmpi/kudrov/smearing_cluster/logs/smearing_test/potential/${monopole}/${conf_type}/${conf_size}/mu${mu}/${chains[j]}"
 mkdir -p ${log_path}
 
 for((i=${conf_start[j]};i<=${conf_end[j]};i++))
@@ -50,26 +55,20 @@ if [ -f ${conf_path} ] ; then
 
 #wilson_path="/home/clusters/rrcmpi/kudrov/smearing_cluster/smearing_test/result/wilson_loop/${conf_type}/${conf_size}/${smearing}/mu${mu}/${chains[j]}"
 #wilson_path="/home/clusters/rrcmpi/kudrov/smearing_cluster/smearing_test/result/wilson_loop/monopoless/${conf_type}/${conf_size}/${smearing}/mu${mu}/${chains[j]}"
-wilson_path="/home/clusters/rrcmpi/kudrov/smearing_cluster/smearing_test/result/wilson_loop/${monopole}/${conf_type}/${conf_size}/${smearing}/mu${mu}/${chains[j]}"
+wilson_path="/home/clusters/rrcmpi/kudrov/smearing_cluster/smearing_test/result/potential/${monopole}/${conf_type}/${conf_size}/${smearing}/mu${mu}/${chains[j]}"
 mkdir -p ${wilson_path}
-wilson_path="${wilson_path}/wilson_loop_${APE_alpha}_$a$b$c$d"
-
-#flux_path="/home/clusters/rrcmpi/kudrov/smearing_cluster/smearing_test/result/flux_tube/${conf_type}/${conf_size}/${smearing}/mu${mu}/${chains[j]}"
-#flux_path="/home/clusters/rrcmpi/kudrov/smearing_cluster/smearing_test/result/flux_tube/monopoless/${conf_type}/${conf_size}/${smearing}/mu${mu}/${chains[j]}"
-flux_path="/home/clusters/rrcmpi/kudrov/smearing_cluster/smearing_test/result/flux_tube/${monopole}/${conf_type}/${conf_size}/${smearing}/mu${mu}/${chains[j]}"
-mkdir -p ${flux_path}
-flux_path="${flux_path}/flux_dep_${APE_alpha}_$a$b$c$d"
+wilson_path="${wilson_path}/wilson_loops_${APE_alpha}_$a$b$c$d"
 
 qsub -q long -v conf_path=${conf_path},conf_format=${conf_format},HYP_alpha1=${HYP_alpha1},HYP_alpha2=${HYP_alpha2},HYP_alpha3=${HYP_alpha3},\
 APE_alpha=${APE_alpha},stout_alpha=${stout_alpha},APE=${APE},HYP=${HYP},stout=${stout},APE_steps=${APE_steps},HYP_steps=${HYP_steps},\
-stout_steps=${stout_steps},flux_enabled=${flux_enabled},wilson_enabled=${wilson_enabled},L_spat=${L_spat},L_time=${L_time},\
-wilson_path=${wilson_path},flux_path=${flux_path},matrix_type=${matrix_type} -o "${log_path}/$a$b$c$d.o" -e "${log_path}/$a$b$c$d.e" /home/clusters/rrcmpi/kudrov/smearing_cluster/scripts/do_dep.sh
+stout_steps=${stout_steps},wilson_enabled=${wilson_enabled},L_spat=${L_spat},L_time=${L_time},T_min=${T_min},T_max=${T_max},R_min=${R_min},R_max=${R_max},calculation_step_APE=${calculation_step_APE}\
+wilson_path=${wilson_path},matrix_type=${matrix_type} -o "${log_path}/$a$b$c$d.o" -e "${log_path}/$a$b$c$d.e" /home/clusters/rrcmpi/kudrov/smearing_cluster/scripts/do_dep.sh
 while [ $? -ne 0 ]
 do
 qsub -q long -v conf_path=${conf_path},conf_format=${conf_format},HYP_alpha1=${HYP_alpha1},HYP_alpha2=${HYP_alpha2},HYP_alpha3=${HYP_alpha3},\
 APE_alpha=${APE_alpha},stout_alpha=${stout_alpha},APE=${APE},HYP=${HYP},stout=${stout},APE_steps=${APE_steps},HYP_steps=${HYP_steps},\
-stout_steps=${stout_steps},flux_enabled=${flux_enabled},wilson_enabled=${wilson_enabled},L_spat=${L_spat},L_time=${L_time},\
-wilson_path=${wilson_path},flux_path=${flux_path},matrix_type=${matrix_type} -o "${log_path}/$a$b$c$d.o" -e "${log_path}/$a$b$c$d.e" /home/clusters/rrcmpi/kudrov/smearing_cluster/scripts/do_dep.sh
+stout_steps=${stout_steps},wilson_enabled=${wilson_enabled},L_spat=${L_spat},L_time=${L_time},T_min=${T_min},T_max=${T_max},R_min=${R_min},R_max=${R_max},calculation_step_APE=${calculation_step_APE}\
+wilson_path=${wilson_path},matrix_type=${matrix_type} -o "${log_path}/$a$b$c$d.o" -e "${log_path}/$a$b$c$d.e" /home/clusters/rrcmpi/kudrov/smearing_cluster/scripts/do_dep.sh
 done
 
 fi
---
+++ scripts/do_dep_potential.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+parameters="-conf_format $conf_format -conf_path $conf_path -HYP_alpha1 $HYP_alpha1 -HYP_alpha2 $HYP_alpha2 -HYP_alpha3 $HYP_alpha3\
+ -APE_alpha $APE_alpha -stout_alpha $stout_alpha -APE $APE -HYP $HYP -stout $stout -HYP_steps $HYP_steps -APE_steps $APE_steps -stout_steps $stout_steps\
+  -L_spat ${L_spat} -L_time ${L_time} -wilson_path ${wilson_path} -wilson_enabled ${wilson_enabled} -T_min ${T_min} -T_max ${T_max} -R_min ${R_min} -R_max ${R_max} -calculation_step_APE ${calculation_step_APE}"
+
+/home/clusters/rrcmpi/kudrov/smearing_cluster/code/exe/smearing_dependence_${matrix_type} $parameters
